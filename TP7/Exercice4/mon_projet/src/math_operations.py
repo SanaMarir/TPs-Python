c53291c314f7cec460695435ed5de7ca52bcf711
SanaMarir <125521813+SanaMarir@users.noreply.github.com>
2025-01-26 00:12:46 +0100

    push EX4

---
+++ TP7/Exercice4/mon_projet/src/math_operations.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("La division par zéro n'est pas permise.")
+    return a / b
